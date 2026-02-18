b3a6c5f73272cf8eac56a027102cf5d741b2e8e3
bthomas279 <benluthomas@gmail.com>
2026-02-18 13:37:27 -0500

    Creating fast API

---
+++ main.py
@@ -0,0 +1,23 @@
+#Code to create API
+from fastapi import FastAPI
+import pickle
+import numpy as np
+from pydantic import BaseModel
+
+#Load ml model
+with open("mental_rating_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI(title="Mental Health Rating API")
+
+class MentalData(BaseModel):
+    study_hours_per_day: float
+    social_media_hours: float
+    netflix_hours: float
+    part_time_job: int
+    sleep_hours: float
+    diet_quality: int
+    exercise_frequency: int
+    extracurricular_participation: int
+
+@app.get("/")
