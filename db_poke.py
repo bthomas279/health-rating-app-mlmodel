5f00f92cfd4eaaeea968ad8ea58a79c10094d25e
bthomas279 <benluthomas@gmail.com>
2026-03-26 22:36:17 -0400

    Still working on data issues

--- db_poke.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-health_db = pd.read_csv("health_dataset.csv")
-
-health_db = health_db.drop(columns=["student_id", "gender", "age", "parental_education_level", 
-                            "attendance_percentage", "internet_quality", "exam_score"])
-
-health_db.to_csv("reduced_health_dataset.csv", index = False)
