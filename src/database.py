0256cf379f47922d37f6b869c4223beadef8c1a3
bthomas279 <benluthomas@gmail.com>
2026-05-05 12:37:38 -0400

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,9 +1,18 @@
 #Code to call the MySQL Database
+import os
 import mysql.connector
+from dotenv import load_dotenv
 
-health_db = mysql.connector.connect(
-    host= ,
-    user= ,
-    password= ,
-    database= ,
-)
+#Load env variables
+load_dotenv()
+def sql_connect():
+    health_db = mysql.connector.connect(
+        host= os.getenv("DB_HOST"),
+        user= os.getenv("DB_USER"),
+        password= os.getenv("DB_PASSWORD"),
+        database= os.getenv("DB_NAME"),
+    ) 
+
+    return health_db  
+
+#Create a callable connection
